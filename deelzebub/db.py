6e9eb560bb9245685efca0e50d96dcce755f8a69
Nicholas Vanhaute <njvanhaute@gmail.com>
2022-02-13 13:13:30 -0600

    Add setup and manifest

--- deelzebub/db.py
+++ deelzebub/db.py
@@ -6,6 +6,7 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
+        sqlite3.enable_callback_tracebacks(True)
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
@@ -23,7 +24,8 @@ def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+        sqlscript = f.read().decode('utf8')
+        db.executescript(sqlscript)
 
 @click.command('init-db')
 @with_appcontext
